0a9aa0de5dc52dc15eefe2a1402574d10e405fbf
patrykiciak <patryk.iciak@gmail.com>
2022-01-14 21:52:58 +0100

    low level approach

--- src/main/kotlin/dk/ucn/jakubzakandrejkutliakpatrykiciak/psuadapter/broker/MessageConsumer.kt
+++ src/main/kotlin/dk/ucn/jakubzakandrejkutliakpatrykiciak/psuadapter/broker/MessageConsumer.kt
@@ -6,34 +6,35 @@ import com.rabbitmq.client.DeliverCallback
 import com.rabbitmq.client.Delivery
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.event.ApplicationStartedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import java.io.IOException
 import java.nio.charset.StandardCharsets
+
 
 @Component
 class MessageConsumer(
     @Value("\${broker.refreshDataRequest}") private val refreshDataRequestQueue: String,
     private val commandProcessor: CommandProcessor,
     connection: Connection
-) : Runnable {
+) {
     private val channel: Channel
     private val logger = LoggerFactory.getLogger(this.javaClass)
-    override fun run() {
+
+    @Async
+    @EventListener(ApplicationStartedEvent::class)
+    fun afterAppStarted() {
         val deliverCallback = DeliverCallback { consumerTag: String?, delivery: Delivery ->
             val message = String(delivery.body, StandardCharsets.UTF_8)
-            println("Received '$message'")
-            logger.info("Received command from \$refreshDataRequestQueue")
+            logger.info("Received command '$message' from ${refreshDataRequestQueue}")
             commandProcessor.process()
         }
-        try {
-            channel.basicConsume(refreshDataRequestQueue, true, deliverCallback) { consumerTag: String? -> }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
+        logger.info("Starting consuming...")
+        channel.basicConsume(refreshDataRequestQueue, true, deliverCallback) { consumerTag: String -> }
     }
-
     init {
         channel = connection.createChannel()
-        channel.queueDeclare(refreshDataRequestQueue, false, false, false, null)
+        channel.queueDeclare(refreshDataRequestQueue, true, false, false, null)
     }
 }
